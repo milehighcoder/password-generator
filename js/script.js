3da9c45a365e10fc839db7651cdf05e881d14335
mykereel <mgmartnz@icloud.com>
2020-10-06 11:13:02 -0600

    prompts working when button is clicked

--- js/script.js
+++ js/script.js
@@ -1,10 +1,23 @@
-// Assignment Code
+// variables declared
+
 var generateBtn = document.querySelector("#generate");
 
-var lowercaseChar =
-var uppercaseChar =
-var numericChar =
-var specialChar =
+// PASSWORD INPUT VARIABLES
+// numeric characters
+var numChar = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+// alphabetical characters
+var abcChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+// special characters
+var specChar = ["!", "#", "$", "%", "&", "*", "@"];
+
+// Prompt that displays in viewport after the user has clicked 'Generate Password'
+function generatePassword() {
+  var passwordLength = prompt("How many characters would you like your password to have? Please choose a range of 8 to 128 characters.");
+  var confirmNum = confirm('Do you want numbers in your password?');
+  var confirmLowerCase = confirm('Do you want lowercase characters in your password?');
+  var confirmUpperCase = confirm('Do you want uppercase characters in your password?');
+  var confirmSpecChar = confirm('Do you want special characters in your password?');
+}
 
 // Write password to the #password input
 function writePassword() {
@@ -12,7 +25,7 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
+ 
 }
 
 // Add event listener to generate button
