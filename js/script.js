921bf6ac46a978eb148a1bf8ec8f930a37b4a152
mykereel <mgmartnz@icloud.com>
2020-10-09 14:15:22 -0600

    invold entry prompt, if/else statements

--- js/script.js
+++ js/script.js
@@ -1,23 +1,67 @@
-// variables declared
 
+// these variables store the initial application values that we will pull from late for the 'for loop'
 var generateBtn = document.querySelector("#generate");
-
-// PASSWORD INPUT VARIABLES
-// numeric characters
 var numChar = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-// alphabetical characters
-var abcChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-// special characters
-var specChar = ["!", "#", "$", "%", "&", "*", "@"];
+var lowerABC = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperABC = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var specChar = ["!", "#", "$", "%", "&", "*", "@", "_"];
+var charArr = [];
 
-// Prompt that displays in viewport after the user has clicked 'Generate Password'
 function generatePassword() {
-  var passwordLength = prompt("How many characters would you like your password to have? Please choose a range of 8 to 128 characters.");
-  var confirmNum = confirm('Do you want numbers in your password?');
-  var confirmLowerCase = confirm('Do you want lowercase characters in your password?');
-  var confirmUpperCase = confirm('Do you want uppercase characters in your password?');
-  var confirmSpecChar = confirm('Do you want special characters in your password?');
+
+  // passwordLength stores the value entered by the user
+  var passwordLength = prompt("How many characters would you like your password to have? Choose a number that is within the range of 8 to 128 characters.");
+  console.log(passwordLength);
+
+  // this 'if' statement returns an alert if the user enters a value that isn't within the specific range
+  if (passwordLength < 8 || passwordLength > 128) {
+    alert("Invalid entry. Enter a value within the specified range.");
+    return "";
+  }
+
+  // these variables store the values of the user's input
+  var confirmNum = confirm("Do you want numbers in your password?");
+  var confirmLowerCase = confirm("Do you want lowercase characters in your password?");
+  var confirmUpperCase = confirm("Do you want uppercase characters in your password?");
+  var confirmSpecChar = confirm("Do you want special characters in your password?");
+
+  // if 'true' the app will concatenate numChar and charArr into a new array
+  if (confirmNum === true){
+    var conOne = charArr.concat(numChar);
+    // if 'false' then the app will concatenate charArr with an empty 'conOne' into a new array
+  } else {
+    var conOne = charArr.concat();
+  }
+
+  if (confirmLowerCase === true){
+    var conTwo = conOne.concat(lowerABC);
+  } else {
+    var conTwo = conOne.concat();
+  }
+
+  if (confirmUpperCase === true){
+    var conThree = conTwo.concat(upperABC);
+  } else {
+    var conThree = conTwo.concat();
+  }
+
+  if (confirmSpecChar === true){
+    var conFour = conFour = conThree.concat(specChar);
+  } else {
+    var conFour = conFour = conThree.concat();
+  }
+  
+  console.log(conFour)
+
+/* NEXT STEPS: Pick out random characters according to the password length requirement.
+Then display the result on the viewport. You can use random.generator to pick our the
+random characters. Use a 'for loop' to pick out the exact password length. We will need
+to use 'math.random()' and 'for (i = 0; i < passwordLength; i++)'.
+ */
+
 }
+
+// return password;
 
 // Write password to the #password input
 function writePassword() {
@@ -25,7 +69,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
- 
 }
 
 // Add event listener to generate button
--- psuedocode.md
+++ psuedocode.md
@@ -1,27 +1,47 @@
-# Password Generator Pseudocode
+# Pseudocode
+
+## Criteria
 
 GIVEN I need a new, secure password
 WHEN I click the button to generate a password
+
 THEN I am presented with a series of prompts for password criteria
 WHEN prompted for password criteria
+
 THEN I select which criteria to include in the password
 WHEN prompted for the length of the password
+
 THEN I choose a length of at least 8 characters and no more than 128 characters
 WHEN prompted for character types to include in the password
+
 THEN I choose lowercase, uppercase, numeric, and/or special characters
 WHEN I answer each prompt
+
 THEN my input should be validated and at least one character type should be selected
 WHEN all prompts are answered
+
 THEN a password is generated that matches the selected criteria
 WHEN the password is generated
+
 THEN the password is either displayed in an alert or written to the page
 
-_Action: user clicks button_
-(have event.listener for user click)
+## Click Event
 
-- prompt for length between 8 & 128 characters. check that the value entered in the first prompt is between 8-128 and alert user to try again if it isn't.
-- store answer as numeric variable (to be used later in for loop for generating password)
-- create arrays for lowercase, numeric, & special character types (use toUpper() on lowercase for upper case array.)
+User clicks 'Generate Password'
+
+## Password Length Prompt
+
+Prompt Text -> 'How many characters would you like your password to have? Please choose a range of 8 to 128 characters.'
+
+Make sure to confirm that the value entered by the user is 8 to 128 characters and alert the user to try again if it isn't.
+
+Alert Text -> 'Character ranges must be withing 8-128 characters. Please try again.'
+
+Store the user's answer as a numeric variable so that it can be used later in 'for loop' that generates the password.
+
+## Password Criteria
+
+Create arrays for lowercase, numeric, & special character types (use toUpper() on lowercase for upper case array.)
 
 _confirm: you will need to chose at least one character type for your password_
 if: yes -
