b64679f07ce33e851803191bb0cc25acdeff8146
mykereel <mgmartnz@icloud.com>
2020-10-10 15:36:38 -0600

    cleaned up comments and formatted code

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 var generateBtn = document.querySelector("#generate");
+
+// declares variables for 'function generatePassword()'
 var numChar = "0123456789";
 var lowerABC = "abcdefghizjklmnopqrstuvwxyz";
 var upperABC = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -8,38 +10,28 @@ var charArr = [];
 // function starts
 function generatePassword() {
   // passwordLength stores the value entered by the user
-  var passwordLength = parseInt(
-    prompt(
-      "How many characters would you like your password to have? Choose a number that is within the range of 8 to 128 characters."
-    )
-  );
-
-  // this 'if' statement returns an alert if the user enters a value that isn't within the specific range
+  var passwordLength = parseInt(prompt("How many characters would you like your password to have? Choose a number that is within the range of 8 to 128 characters."));
+  // this 'if' statement returns an alert if the user enters a value that isn't within the specific range.
   if (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
     alert("Invalid entry. Enter a value within the specified range.");
     return "";
   }
 
-  // these variables store the values of the user's input
+  // these variables store boolean values in order to determine the character type preferences of the user
   var confirmNum = confirm("Do you want numbers in your password?");
-  var confirmLowerCase = confirm(
-    "Do you want lowercase characters in your password?"
-  );
-  var confirmUpperCase = confirm(
-    "Do you want uppercase characters in your password?"
-  );
-  var confirmSpecChar = confirm(
-    "Do you want special characters in your password?"
-  );
+  var confirmLowerCase = confirm("Do you want lowercase characters in your password?");
+  var confirmUpperCase = confirm("Do you want uppercase characters in your password?");
+  var confirmSpecChar = confirm("Do you want special characters in your password?");
 
-  // if 'true' the app will concatenate numChar and charArr into a new array
+  // start concatenation
+  // if 'true' the app will concatenate charArr and numChar. If 'false' then the app will concatenate charArr with an empty string.
   if (confirmNum === true) {
     var conOne = charArr.concat(numChar);
-    // if 'false' then the app will concatenate charArr with an empty 'conOne' array into a new array
   } else {
     var conOne = charArr.concat();
   }
 
+  // IMPORTANT: the concatenation process above (lines 28-32) is repeated for lines 35 to 51 in order to stack the user's character type preferences into one final array (conFour) for the 'for loop'
   if (confirmLowerCase === true) {
     var conTwo = conOne.concat(lowerABC);
   } else {
@@ -57,26 +49,28 @@ function generatePassword() {
   } else {
     var conFour = (conFour = conThree.concat());
   }
+  // end concatenation
 
+  // this 'if' statement validates that at least one character type was selected by the user
   if (confirmNum != true && confirmLowerCase != true && confirmUpperCase != true && confirmSpecChar != true) {
-    alert('Must select at least one character type.');
+    alert("Invalid preferences. Please select at least one character type.");
   }
 
+  // changes the conFour array into a string
   var conFourString = conFour.toString();
-
+  // assigns an empty value to 'result' so that a random value can be returned to it
   var result = "";
 
-  // takes the length of the
+  // this for loop allows us to access the password length preference set by the user
   for (i = 0; i < passwordLength; i++) {
-    var randomPassword = conFourString.charAt(
-      Math.floor(Math.random() * conFourString.length)
-    );
-    console.log(randomPassword);
+    // generates random characters in the amount of passwordLength
+    var randomPassword = conFourString.charAt(Math.floor(Math.random() * conFourString.length));
+    // concatenates the random characters generated in line 66 into a string
     var result = randomPassword.concat(result);
-    console.log(result);
   }
-
+  
   return result;
+
 }
 // function end
 
