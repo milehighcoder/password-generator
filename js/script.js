0eaeff80a921b4eb99d1c23686d5bb9b87ffa122
mykereel <mgmartnz@icloud.com>
2020-10-10 14:49:32 -0600

    added validation for at least one character selected

--- js/script.js
+++ js/script.js
@@ -1,71 +1,83 @@
-
-// these variables store the initial application values that we will pull from late for the 'for loop'
 var generateBtn = document.querySelector("#generate");
-var numChar = '0123456789';
-var lowerABC = 'abcdefghizjklmnopqrstuvwxyz';
-var upperABC = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-var specChar = '!#$%&*@_';
+var numChar = "0123456789";
+var lowerABC = "abcdefghizjklmnopqrstuvwxyz";
+var upperABC = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var specChar = "!#$%&*@_";
 var charArr = [];
 
 // function starts
 function generatePassword() {
-
   // passwordLength stores the value entered by the user
-  var passwordLength = parseInt(prompt("How many characters would you like your password to have? Choose a number that is within the range of 8 to 128 characters."));
+  var passwordLength = parseInt(
+    prompt(
+      "How many characters would you like your password to have? Choose a number that is within the range of 8 to 128 characters."
+    )
+  );
 
   // this 'if' statement returns an alert if the user enters a value that isn't within the specific range
-  if (passwordLength < 8 || passwordLength > 128) {
+  if (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
     alert("Invalid entry. Enter a value within the specified range.");
     return "";
   }
 
   // these variables store the values of the user's input
   var confirmNum = confirm("Do you want numbers in your password?");
-  var confirmLowerCase = confirm("Do you want lowercase characters in your password?");
-  var confirmUpperCase = confirm("Do you want uppercase characters in your password?");
-  var confirmSpecChar = confirm("Do you want special characters in your password?");
+  var confirmLowerCase = confirm(
+    "Do you want lowercase characters in your password?"
+  );
+  var confirmUpperCase = confirm(
+    "Do you want uppercase characters in your password?"
+  );
+  var confirmSpecChar = confirm(
+    "Do you want special characters in your password?"
+  );
 
   // if 'true' the app will concatenate numChar and charArr into a new array
-  if (confirmNum === true){
+  if (confirmNum === true) {
     var conOne = charArr.concat(numChar);
     // if 'false' then the app will concatenate charArr with an empty 'conOne' array into a new array
   } else {
     var conOne = charArr.concat();
   }
 
-  if (confirmLowerCase === true){
+  if (confirmLowerCase === true) {
     var conTwo = conOne.concat(lowerABC);
   } else {
     var conTwo = conOne.concat();
   }
 
-  if (confirmUpperCase === true){
+  if (confirmUpperCase === true) {
     var conThree = conTwo.concat(upperABC);
   } else {
     var conThree = conTwo.concat();
   }
 
-  if (confirmSpecChar === true){
-    var conFour = conFour = conThree.concat(specChar);
+  if (confirmSpecChar === true) {
+    var conFour = (conFour = conThree.concat(specChar));
   } else {
-    var conFour = conFour = conThree.concat();
+    var conFour = (conFour = conThree.concat());
+  }
+
+  if (confirmNum != true && confirmLowerCase != true && confirmUpperCase != true && confirmSpecChar != true) {
+    alert('Must select at least one character type.');
   }
 
   var conFourString = conFour.toString();
 
-  var result = '';
+  var result = "";
 
+  // takes the length of the
   for (i = 0; i < passwordLength; i++) {
-    var randomPassword = conFourString.charAt(Math.floor(Math.random() * conFourString.length));
-    console.log(randomPassword)
+    var randomPassword = conFourString.charAt(
+      Math.floor(Math.random() * conFourString.length)
+    );
+    console.log(randomPassword);
     var result = randomPassword.concat(result);
     console.log(result);
   }
 
   return result;
-
 }
-
 // function end
 
 // Write password to the #password input
